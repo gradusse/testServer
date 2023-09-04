1336c01ba55874e3d049a87cdef1f64cf6af5a94
gradusse <vlad141098@mail.ru>
2023-09-04 15:21:55 +0200

    remove node modules

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+let requestCount = 0;
+
+const server = http.createServer((request, response) => {
+
+    requestCount++;
+
+    switch (request.url) {
+        case '/students':
+            response.write('STUDENTS');
+            break;
+        case '/courses' :
+            response.write('FRONT + BACK');
+            break;
+        default:
+            response.write(' 404 not found');
+    };
+    response.end();
+});
+
+server.listen(3003);
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express, { Express, Request, Response } from 'express';
+//const express = require('express');
+const app = express();
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+// комментарий
+app.get("/", function (req: Request, res: Response) {
+  req.header('Content-Type');
+  res.statusCode = 200;
+  res.set('Content-type', 'application/json');
+  res.send({
+    firstName: "Иван",
+    lastName: "Иванов",
+    address: {
+      streetAddress: "Московское ш., 101, кв.101",
+      city: "Ленинград",
+      postalCode: 101101
+    },
+    phoneNumbers: [
+      "812 123-1234",
+      "916 123-4567"
+    ]
+  });
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
